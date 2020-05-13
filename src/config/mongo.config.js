8cf69fd748ca28c85e1edeb071726b461f07ab3b
DevelopmentByDavid <developmentbydavid@gmail.com>
2020-05-13 11:54:11 -0700

    finished?

--- src/config/mongo.config.js
+++ src/config/mongo.config.js
@@ -5,32 +5,11 @@ require('dotenv').config();
 const { DB_URL } = process.env;
 const dbName = 'prytaneum-auth';
 
-// const client = MongoClient.connect(DB_URL, { useUnifiedTopology: true }).catch(
-//     console.err
-// ); // TODO: real logging here
-
-// export default class MongoCollection {
-//     constructor(name) {
-//         this.name = name;
-//         this.connect();
-//     }
-
-//     async connect() {
-//         try {
-//             await client.db(dbName).collection(this.name);
-//             return this;
-//         } catch (e) {
-//             return e;
-//         }
-//     }
-// }
+const client = new MongoClient(DB_URL, { useUnifiedTopology: true });
+const connected = client.connect().then((connection) => connection.db(dbName));
 
 export default function MongoCollection(name) {
-    const client = new MongoClient(DB_URL, { useUnifiedTopology: true });
-    // client.connect((connected) => {
-    //     const connected.db(dbName);
-    // });
-    client.connect().then((connected) => {
-        this.collection = connected.db(dbName).collection(name);
+    connected.then((db) => {
+        this.collection = db.collection(name);
     });
 }
--- src/db/users.js
+++ src/db/users.js
@@ -1,51 +1,45 @@
 import { ObjectID } from 'mongodb';
 import MongoCollection from '../config/mongo.config';
 
-const Users = new MongoCollection('users');
+const Users = function () {
+    MongoCollection.call(this, 'users');
+};
 
-const addUser = function (userDoc) {
+Users.prototype.addUser = function (userDoc) {
     return this.collection.insertOne(userDoc).then((r) => r.ops[0]);
 };
 
-const updateUser = function (doc, addition) {
+Users.prototype.updateUser = function (doc, addition) {
     return this.collection.updateOne(doc, addition);
 };
 
-const removeUser = function (_id) {
+Users.prototype.removeUser = function (_id) {
     return this.collection.deleteOne({ _id: new ObjectID(_id) });
 };
 
-const findByUsername = function ({ username }) {
+Users.prototype.findByUsername = function ({ username }) {
     return this.collection
         .find({ username })
         .toArray()
         .then((r) => r[0]);
 };
 
-const findByUserId = function (userId) {
+Users.prototype.findByUserId = function (userId) {
     return this.collection
         .find({ _id: new ObjectID(userId) })
         .toArray()
         .then((r) => r[0]);
 };
 
-const findByEmail = function (email) {
+Users.prototype.findByEmail = function (email) {
     return this.collection
         .find({ email })
         .toArray()
         .then((r) => r[0]);
 };
 
-const find = async function (...args) {
+Users.prototype.find = async function (...args) {
     this.collection.find(...args).toArray();
 };
 
-export default {
-    find: find.bind(Users),
-    findByEmail: findByEmail.bind(Users),
-    findByUserId: findByUserId.bind(Users),
-    findByUsername: findByUsername.bind(Users),
-    updateUser: updateUser.bind(Users),
-    addUser: addUser.bind(Users),
-    removeUser: removeUser.bind(Users),
-};
+export default new Users();
