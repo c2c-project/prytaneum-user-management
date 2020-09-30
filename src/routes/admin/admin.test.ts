87ace8e019d793934098b0005e1018cd7d0d5fee
Francisco Gallego <fgall002@ucr.edu>
2020-09-30 11:10:06 -0700

    Added Admin backend funcionality

---
+++ src/routes/admin/admin.test.ts
@@ -0,0 +1,86 @@
+import { ObjectID } from 'mongodb';
+import request from 'supertest';
+import faker from 'faker';
+
+import app from 'app';
+import Users from 'modules/users';
+import Collections, { connect, close } from 'db';
+
+const statusType = ['Admin', 'Moderator', 'Organizer', 'Attended', 'Banned'];
+
+// TODO Change Id used as a parameter in url
+
+const makeUserStatus = statusType.map((s) => {
+    return {
+        role: s,
+        count: faker.random.number(),
+        active: faker.random.boolean(),
+    };
+});
+
+const mockUser = {
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    userRoles: ['users'],
+    _id: new ObjectID(),
+    status: makeUserStatus,
+};
+
+beforeAll(async () => {
+    await connect();
+    const { insertedId } = await Users.register(
+        mockUser.email,
+        mockUser.password,
+        mockUser.password
+    );
+    mockUser._id = insertedId;
+});
+
+afterAll(async () => {
+    await Collections.Users().deleteOne({ 'email.address': mockUser.email });
+    await close();
+});
+
+describe('#User List', () => {
+    const url = '/api/users/list';
+
+    it('it should retrieve a list of user for admin to see', async () => {
+        const { status } = await request(app).get(url);
+        expect(status).toStrictEqual(200);
+    });
+
+    it('it should not retrieve al ist of user, invalid connection', async () => {
+        const { status } = await request(app).get(`${url}x`);
+        expect(status).toStrictEqual(400);
+    });
+});
+
+describe('#Fetch specific User', () => {
+    const url = '/api/users/:userId';
+    it('it should retrieve info of a specific user', async () => {
+        const { status } = await request(app)
+            .get(url)
+            .query({ userId: mockUser._id });
+
+        expect(status).toStrictEqual(200);
+    });
+});
+
+describe('#Promote a specific User', () => {
+    const url = '/users/:userId/update';
+
+    it('should promote a user', async () => {
+        const userStatus = makeUserStatus;
+
+        const { status } = await request(app)
+            .patch(url)
+            .query({ userId: mockUser._id })
+            .send({
+                form: {
+                    status: userStatus,
+                },
+            });
+
+        expect(status).toStrictEqual(200);
+    });
+});
--- src/routes/admin/admin.ts
+++ src/routes/admin/admin.ts
@@ -1,25 +1,93 @@
+/* eslint-disable @typescript-eslint/indent */
 import express from 'express';
 import Joi from '@hapi/joi';
 
-import { isAdminMiddleware } from 'modules/admin';
+import Admin from 'modules/admin';
+import { userForm, userStatus } from 'modules/admin/types';
+
 import { makeJoiMiddleware } from 'middlewares';
 
 const router = express.Router();
 
-interface RegForm {
-    email: string;
-    password: string;
-    confirmPassword: string;
-}
-
-router.use(isAdminMiddleware);
+router.use(Admin.isAdminMiddleware);
 /* TODO: add code here for admin dashboard
     1. user list
-    2. promote user
-    3. get one specific user
+    2. get one specific user
+    3. promote user
 */
-router.post('/users/list', () => {
-    // do something
+
+// *Fetch all the user data
+router.get('/users/list', async (req, res, next) => {
+    try {
+        const users = await Admin.fetchAllUsers();
+        res.status(200).send(users);
+    } catch (e) {
+        next(e);
+    }
 });
+
+// *Fetch A specific user data
+router.get(
+    '/users/:userId',
+    makeJoiMiddleware({
+        params: Joi.object({
+            userId: Joi.string()
+                .required()
+                .messages({ 'any.required': 'Invalid Link' }),
+        }),
+    }),
+    async (req, res, next) => {
+        try {
+            const { userId } = req.params;
+            const user = await Admin.confirmUserExist(userId);
+            if (user) {
+                res.status(200).send(user);
+            }
+        } catch (e) {
+            next(e);
+        }
+    }
+);
+
+// *Patch a user status data. Allows modification of user status
+router.patch(
+    '/users/:userId/update',
+    makeJoiMiddleware({
+        params: Joi.object({
+            userId: Joi.string()
+                .required()
+                .messages({ 'any.required': 'Invalid link' }),
+        }),
+        body: Joi.object({
+            form: Joi.object<userForm, userForm>({
+                status: Joi.array()
+                    .items(
+                        Joi.object<userStatus, userStatus>({
+                            role: Joi.string().required().messages({
+                                'any.required': 'valid status required',
+                            }),
+                            count: Joi.number().required().messages({
+                                'number.required': 'Valid number is required',
+                            }),
+                            active: Joi.boolean().required().messages({
+                                'any.required': 'Boolean value was not passed',
+                            }),
+                        }).required()
+                    )
+                    .required(),
+            }).required(),
+        }),
+    }),
+    async (req, res, next) => {
+        try {
+            const { userId } = req.params;
+            const { form } = req.body as { form: { status: userStatus[] } };
+            await Admin.updateUserStatus(userId, form.status);
+            res.status(200).send();
+        } catch (e) {
+            next(e);
+        }
+    }
+);
 
 export default router;
